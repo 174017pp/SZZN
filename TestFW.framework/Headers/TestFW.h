1686a17f50f80c4cb01c431162daa062c5745759
174017pp <411578012@qq.com>
2018-12-21 16:29:46 +0800

    Add files via upload

---
+++ TestFW.framework/Headers/TestFW.h
@@ -0,0 +1,19 @@
+//
+//  TestFW.h
+//  TestFW
+//
+//  Created by admin on 2018/12/6.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//! Project version number for TestFW.
+FOUNDATION_EXPORT double TestFWVersionNumber;
+
+//! Project version string for TestFW.
+FOUNDATION_EXPORT const unsigned char TestFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFW/PublicHeader.h>
+#import "TestObject.h"
+
+
---
+++ TestFW.framework/Headers/TestObject.h
@@ -0,0 +1,18 @@
+//
+//  TestObject.h
+//  FrameworkProject
+//
+//  Created by admin on 2018/12/6.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestObject : NSObject
+- (void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
